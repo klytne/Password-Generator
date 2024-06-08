c57f893657d1c1c6a2a511ab310ae92e6856ffbf
klytne <tienganekelly@gmail.com>
2024-06-08 18:22:24 +0200

    Create password function

--- script.js
+++ script.js
@@ -1,16 +1,55 @@
-document.addEventListener("DOMContentLoader", function() {
+/*document.addEventListener("DOMContentLoader", function() {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@$%^&*+#'
+    
     const length = document.querySelector("#input").value
-    const btnEl = document.querySelector(".btn")
+    const btnEl = document.querySelector("#btn")
+
     let password = ""
 
-    btnEl.addEventListener("click", function() {
-        for(let i = 0; i < length; i++) {
-            let ranNum = Math.floor(Math.random() * chars.length)
-            password += chars[ranNum]
-        }
-    })
+    if(btnEl != null) {
+        btnEl.addEventListener("click", function() {
+            for(let i = 0; i < length; i++) {
+                let ranNum = Math.floor(Math.random() * chars.length)
+                password += chars[ranNum]
+            }
+        })
+    } else {
+        console.error("And then!")
+    }
 
     let c = document.querySelector(".pass1")
     c.textContent = password
+})*/
+
+function genePass(len) {
+    let password = ""
+
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@$%^&*+#'
+
+        for(let i = 0; i < len; i++) {
+            let ranNum = Math.floor(Math.random() * chars.length)
+            password += chars[ranNum]
+        }
+    
+        //document.querySelector(".pass1").textContent = password
+
+    return password
+}
+
+document.addEventListener('click', function() {
+    document.querySelector("#btn").addEventListener('click', function() {
+        //let length = 0
+        //console.log(length)
+
+        let length = document.querySelector("#input").value
+        //console.log(length)
+
+        let pass2 = genePass(length)
+
+        document.querySelector("#pass2").textContext = pass2
+
+        length = 0
+        //console.log(length)
+    })
+
 })
